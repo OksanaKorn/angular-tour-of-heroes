852ac228d89c92a76ffc79f0e75eebc639efc74d
OksanaKorn <kskorni21@gmail.com>
2017-10-31 22:05:38 +0200

    add a styles to all project

--- src/app/heroes.component.ts
+++ src/app/heroes.component.ts
@@ -5,37 +5,35 @@ import { Hero } from './hero';
 import { HeroService } from './hero.service';
 
 
-
-
 @Component({
- selector: 'my-heroes',
- templateUrl: './heroes.component.html',
- styleUrls: [ './heroes.component.css' ]
+  selector: 'my-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: [ './heroes.component.css' ]
 })
+
 export class HeroesComponent implements OnInit {
-
- heroes: Hero[];
- selectedHero: Hero;
+  heroes: Hero[];
+  selectedHero: Hero;
  
- constructor(
-   private router: Router,
-   private heroService: HeroService
-   ) { }
-
- getHeroes(): void {
-   this.heroService.getHeroes().then(heroes => this.heroes = heroes);
- }
- ngOnInit(): void {
-  this.getHeroes();
+  constructor(
+    private router: Router,
+    private heroService: HeroService) { }
+ 
+  getHeroes(): void {
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+  }
+ 
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+ 
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+ 
+  gotoDetail(): void {
+    this.router.navigate(['/detail', this.selectedHero.id]);
+  }
 }
-
- onSelect(hero: Hero): void {
-   this.selectedHero = hero;
- }
-
- gotoDetail(): void {
-   this.router.navigate(['./detail', this.selectedHero.id]);
- }
- };
 
 

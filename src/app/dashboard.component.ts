053f7c85cd276ae6bc876c4b53cb4b8eb5e96bd1
OksanaKorn <kskorni21@gmail.com>
2017-10-31 22:10:42 +0200

    add a routerLink to dashboard.html

--- src/app/dashboard.component.ts
+++ src/app/dashboard.component.ts
@@ -4,18 +4,18 @@ import { Hero } from './hero';
 import { HeroService } from './hero.service';
 
 @Component({
-    selector: 'my-dashboard',
-    templateUrl: './dashboard.component.html',
-    styleUrls: ['./dashboard.component.css']
+  selector: 'my-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: [ './dashboard.component.css' ]
 })
-
 export class DashboardComponent implements OnInit {
-    heroes: Hero[] = [];
 
-    constructor(private heroService: HeroService) {}
+  heroes: Hero[] = [];
 
-    ngOnInit(): void {
-        this.heroService.getHeroes()
-        .then(heroes => this.heroes = heroes.slice(1, 5));
-    }
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit(): void {
+    this.heroService.getHeroes()
+      .then(heroes => this.heroes = heroes.slice(1, 5));
+  }
 }
